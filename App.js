540c840846800e40b4da72ddb93fe4b0edf106a8
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-11 02:01:25 +0530

    Basic UI for Balck Jack game

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-let firstcard = 12;
-let secondcard = 10;
+let firstcard = 11;
+let secondcard = 9;
 let sum = firstcard + secondcard;
+let hasBlackJack = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if (sum < 21) {
-  console.log("Do you want to draw a new card? 🙂");
-} else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack! 🥳");
-} else if (sum > 21) {
-  console.log("You're out of the game! 😭");
+function startGame() {
+  cardsEl.textContent = "cards:" + firstcard + " " + secondcard;
+  sumEl.textContent = "Sum:" + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+  }
+
+  messageEl.textContent = message;
+}
+function newCard() {
+
+  let card = 1;
+  sum = sum + card;
+  startGame();
 }
