89622b0430e38efdc4681764a04ee8fd9c0b753a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-22 00:46:11 +0530

    Updating game

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-let firstcard = 11;
-let secondcard = 9;
-let sum = firstcard + secondcard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+  let randomNumer = Math.floor(Math.random() * 13) + 1;
+  if (randomNumer > 10) {
+    return 10;
+  } else if (randomNumer === 1) {
+    return 11;
+  } else {
+    return randomNumer;
+  }
+}
 function startGame() {
-  cardsEl.textContent = "cards:" + firstcard + " " + secondcard;
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+function renderGame() {
+  cardsEl.textContent = "cards:";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
   sumEl.textContent = "Sum:" + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂";
@@ -22,7 +43,9 @@ function startGame() {
   messageEl.textContent = message;
 }
 function newCard() {
-  let card = 1;
+  let card = getRandomCard();
   sum = sum + card;
-  startGame();
+  cards.push(card);
+  console.log(cards);
+  renderGame();
 }
